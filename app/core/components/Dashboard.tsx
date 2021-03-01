3e0076ed30aa6087148bb1de5292b42462c50510
Matt Wood <matt@cratebind.com>
2021-03-01 11:25:06 -0600

    Add delete mutation

--- app/core/components/Dashboard.tsx
+++ app/core/components/Dashboard.tsx
@@ -8,6 +8,7 @@ import logout from 'app/auth/mutations/logout';
 import createEntry from 'app/entries/mutations/createEntry';
 import getEntries from 'app/entries/queries/getEntries';
 import updateEntry from 'app/entries/mutations/updateEntry';
+import deleteEntry from 'app/entries/mutations/deleteEntry';
 
 /*
   This example requires Tailwind CSS v2.0+
@@ -34,12 +35,21 @@ const EntryItem = ({
   refetch: () => void;
 }) => {
   const [updateEntryMutation] = useMutation(updateEntry);
+  const [deleteEntryMutation] = useMutation(deleteEntry);
 
   async function handleUpdate(updates) {
     await updateEntryMutation({
       id: entry.id,
       ...updates,
     });
+    await refetch();
+  }
+
+  async function handleDelete() {
+    await deleteEntryMutation({
+      id: entry.id,
+    });
+
     await refetch();
   }
 
@@ -129,51 +139,57 @@ const EntryItem = ({
               >
                 <Menu.Items static>
                   <div className="py-1" role="none">
-                    <a
-                      href="#"
-                      className="group flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-                      role="menuitem"
-                    >
-                      {/* Heroicon name: solid/pencil-alt */}
-                      <svg
-                        className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
+                    <Menu.Item disabled>
+                      <a
+                        href="#"
+                        className="group flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                        role="menuitem"
                       >
-                        <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-                        <path
-                          fillRule="evenodd"
-                          d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                      Edit
-                    </a>
+                        <span className="sr-only">Edit Entry</span>
+                        {/* Heroicon name: solid/pencil-alt */}
+                        <svg
+                          className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                          <path
+                            fillRule="evenodd"
+                            d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                        Edit
+                      </a>
+                    </Menu.Item>
                   </div>
                   <div className="py-1" role="none">
-                    <a
-                      href="#"
-                      className="group flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-                      role="menuitem"
-                    >
-                      {/* Heroicon name: solid/trash */}
-                      <svg
-                        className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
+                    <Menu.Item onClick={async () => await handleDelete()}>
+                      <a
+                        href="#"
+                        className="group flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                        role="menuitem"
                       >
-                        <path
-                          fillRule="evenodd"
-                          d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                      Delete
-                    </a>
+                        <span className="sr-only">Delete Entry</span>
+                        {/* Heroicon name: solid/trash */}
+                        <svg
+                          className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                        Delete
+                      </a>
+                    </Menu.Item>
                   </div>
                 </Menu.Items>
               </Transition>
@@ -928,7 +944,7 @@ export default function Dashboard() {
             </div>
             <ul className="divide-y divide-gray-100">
               {entries?.map((entry) => (
-                <EntryItem entry={entry} refetch={refetch} />
+                <EntryItem key={entry.id} entry={entry} refetch={refetch} />
               ))}
               {/* More items... */}
             </ul>
